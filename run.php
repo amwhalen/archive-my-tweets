143d6bb11e362f1edbbd5637315775379b7af182
Théophile Helleboid - chtitux <chtitux@gmail.com>
2013-12-09 00:01:46 +0100

    Set theme to atom if atom=1 is present in URL

--- run.php
+++ run.php
@@ -4,6 +4,10 @@ if (file_exists(dirname(__FILE__).'/config.php')) {
 	
 	require_once('includes.php');
 	$config = (isset($config)) ? $config : array();
+	if(isset($_GET) && $_GET['atom'] == "1")
+	{
+		$config['theme'] = 'atom';
+	}
 	$amt = new \AMWhalen\ArchiveMyTweets\App($config);
 	$amt->run();
 	
