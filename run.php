41361e59db4ed4e50638dc43709de4b0d6d3f842
MKody <im@kdy.ch>
2016-01-29 09:03:50 +0100

    Fix `NOTICE` message.

--- run.php
+++ run.php
@@ -1,16 +1,16 @@
 <?php if (!defined('ARCHIVE_MY_TWEETS')) exit('No direct access allowed.');
 
 if (file_exists(dirname(__FILE__).'/config.php')) {
-	
+
 	require_once('includes.php');
 	$config = (isset($config)) ? $config : array();
-	if(isset($_GET) && $_GET['atom'] == "1")
+	if(isset($_GET['atom']) && $_GET['atom'] == "1")
 	{
 		$config['theme'] = 'atom';
 	}
 	$amt = new \AMWhalen\ArchiveMyTweets\App($config);
 	$amt->run();
-	
+
 } else {
 
 	require_once('amt/installer.php');
